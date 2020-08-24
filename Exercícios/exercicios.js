310a10e01c62c3cbee971c96223d3f3fded3abf7
MathWer <mat-wer@hotmail.com>
2020-08-23 23:39:13 -0300

    Ajeitei o gerador.

--- Exercícios/exercicios.js
+++ Exercícios/exercicios.js
@@ -1,16 +1,15 @@
-function fibonacciGenerator (n) {
-   
-    var arr=[]
-      numA = 0
-      numB = 1
-      for(i=2; i<=n; i++){
-          arr.push(numA)
-          numB+=numA
-          numA+=numB
-          arr.push(numB)
-      }
-      arr.length = (n-1)/2
-      console.log(arr)
-  }
-  
-  fibonacciGenerator(8)
+function geradorFibonacci(n) {
+
+    var arr = []
+    a = 0
+    b = 1
+    for (i = 0; i < n; i++) {
+        arr.push(a)
+        var temp = a
+        a += b
+        b = temp
+    }
+    console.log(arr)
+}
+
+geradorFibonacci(10)
