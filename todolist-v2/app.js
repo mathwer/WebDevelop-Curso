905c808f09a535873a5c7e7a2afb403e1fc1e309
MathWer <mat-wer@hotmail.com>
2020-10-12 17:32:50 -0300

    Finalizado para rodar no heroku

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -158,6 +158,8 @@ app.get("/about", function (req, res) {
 });
 
 
-app.listen(3000, function () {
-  console.log("Servidor iniciado na porta 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
